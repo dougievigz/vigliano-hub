d8cf7c5f21ab9ffb8170d1e34d8f86a645594184
Rizzbot Vigz <rizzbotvigz@Rizzbots-Mac-mini.local>
2026-03-05 18:21:55 -0600

    Fix: Wrap password component in ConfigProvider for proper Ant Design styling

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { ConfigProvider } from 'antd'
+import { RefineThemes } from '@refinedev/antd'
 import 'antd/dist/reset.css'
+import '@refinedev/antd/dist/reset.css'
 import './index.css'
 import App from './App.tsx'
 import { PasswordProtect } from './components/PasswordProtect'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <PasswordProtect>
-      <App />
-    </PasswordProtect>
+    <ConfigProvider theme={RefineThemes.Blue}>
+      <PasswordProtect>
+        <App />
+      </PasswordProtect>
+    </ConfigProvider>
   </StrictMode>,
 )
